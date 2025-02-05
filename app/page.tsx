3170c2a6b771f696ba6e8f2ade51c780e5ed991b
Rasel Parvez Sanny <raselparvezsanny@gmail.com>
2025-02-05 17:15:41 +0600

    remove: deleted old components for new design

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,7 @@
-import AboutMe from '@/components/AboutMe';
-import FeaturedProject from '@/components/FeaturedProject';
-import Sidebar from '@/components/Sidebar';
-import TechStack from '@/components/TechStack';
+
 
 export default function Home() {
   return (
-    <div className="bg-background min-h-screen">
-      <div className="grid grid-cols-1 md:grid-cols-3 mx-5 md:mx-32 py-8 gap-8">
-        <div>
-          <Sidebar />
-        </div>
-        <main className="md:col-span-2">
-          <AboutMe />
-          <FeaturedProject />
-          <TechStack />
-        </main>
-      </div>
-    </div>
+    <div>Home</div>
   );
 }
