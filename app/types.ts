fae56e64a964c2873f6df3dea35e35fa44e060c1
Rasel Parvez Sanny <raselparvezsanny@gmail.com>
2025-02-05 17:50:31 +0600

    add: types component created

---
+++ app/types.ts
@@ -0,0 +1,44 @@
+// types.ts
+import { ReactElement } from 'react';
+import { LucideIcon } from 'lucide-react';
+
+export interface TerminalHistoryItem {
+  command: string;
+  output: string[];
+}
+
+export interface Project {
+  title: string;
+  description: string;
+  tags: string[];
+  icon: LucideIcon;
+}
+
+export interface CursorProps {
+  position: { x: number; y: number };
+}
+
+export interface BackgroundPatternProps {
+  pattern: string;
+}
+
+export interface TerminalProps {
+  isOpen: boolean;
+  onClose: () => void;
+  terminalHistory: TerminalHistoryItem[];
+  currentCommand: string;
+  setCurrentCommand: (command: string) => void;
+  handleCommand: (command: string) => void;
+}
+
+export interface ProjectCardProps {
+  project: Project;
+  pattern: string;
+  onHover: () => void;
+}
+
+export interface ContactLinkProps {
+  label: string;
+  icon: LucideIcon;
+  pattern: string;
+}
